32e6d0fadb890dfb36647a6e8ba840e10295920d
NiTRONDC1 <84825812+NiTRONDC@users.noreply.github.com>
2021-07-09 19:42:42 +0530

    Update pmpermit.py

--- userge/plugins/utils/pmpermit.py
+++ userge/plugins/utils/pmpermit.py
@@ -295,7 +295,7 @@ async def uninvitedPmHandler(message: Message):
         PMPERMIT_MSG[message.from_user.id] = (
             await message.reply(
                 noPmMessage.format_map(SafeDict(**user_dict))
-                + "\n`- Protected by USERGE-X`"
+                + "\n\n`─➤ Protected by USERGE-X  𒅒`"
             )
         ).message_id
         await asyncio.sleep(1)
